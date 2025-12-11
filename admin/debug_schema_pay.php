84a80324497578c5f4a08dec49bc09be81df3846
Willhelmus Juaness Patudang <willyjuaness@gmail.com>
2025-12-11 10:02:46 +0800

    Clean up project: remove debug, test, and documentation files

--- admin/debug_schema_pay.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../config/db.php';
-$res = $conn->query("DESCRIBE pembayaran");
-echo "<table border=1><tr><th>Field</th><th>Type</th></tr>";
-while ($row = $res->fetch_assoc()) {
-    echo "<tr><td>{$row['Field']}</td><td>{$row['Type']}</td></tr>";
-}
-echo "</table>";
-?>
--- check_kost_table.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'config/db.php';
-$result = $conn->query('DESCRIBE kost');
-echo "Kost table structure:\n";
-while($row = $result->fetch_assoc()) {
-    echo $row['Field'] . ' - ' . $row['Type'] . "\n";
-}
